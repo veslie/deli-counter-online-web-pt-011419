77da2f1ae0a1cb3d5c88718ac1de7af03a3eaeaf
veslie losius <veslielosius@yahoo.com>
2019-02-08 03:02:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli = []
+
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "kent")
+ line(katz_deli)
+ 
+ now_serving(katz_deli)
+ 
+ line(katz_deli)
+ 
+take_a_number(katz_deli, "Martz")
+ 
+ line(katz_deli)
+now_serving(katz_deli)
+ line(katz_deli)
+ 
+
